01076c37d00040ecddb8399f8529edad035efc92
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-10 04:54:43 +0900

    feat: ResponseStatus 수정
    - BAD REQUEST를 추가했습니다.
    - HttpStatus의 value를 반환하는 getHttpStatusValue()를 추가했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
@@ -2,20 +2,21 @@ package com.hymin.webtoon_review.global.response;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ResponseStatus {
-    OK(HttpStatus.OK, "정상 처리 됐습니다.");
+    OK(HttpStatus.OK, "정상 처리 됐습니다."),
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
 
-    public int getStatus() {
+    public int getHttpStatusValue() {
         return httpStatus.value();
-    }
-
-    public String getMessage() {
-        return message;
     }
 }

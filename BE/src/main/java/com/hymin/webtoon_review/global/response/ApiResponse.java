60a8924a39ce12a32aa24fbf3379d292d20fcdb0
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-10 04:58:15 +0900

    refactor: ApiResponse 수정
    - RestResponse를 상속받도록 수정했습니다.
    - data를 제외한 기존 필드를 삭제했습니다.
    - 생성자를 추가했습니다.
    - of()와 onSuccess()를 현재에 맞게 수정했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/response/ApiResponse.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/ApiResponse.java
@@ -1,34 +1,26 @@
 package com.hymin.webtoon_review.global.response;
 
-import com.hymin.webtoon_review.util.Time;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class ApiResponse<T> {
+public class ApiResponse<T> extends RestResponse {
 
-    private Integer status;
-    private String message;
-    private String time;
     private T data;
+
+    protected ApiResponse(Integer status, String message, T data) {
+        super(status, message);
+        this.data = data;
+    }
 
     public static <T> ApiResponse<T> onSuccess(T data) {
         return ApiResponse.of(ResponseStatus.OK, data);
     }
 
     public static <T> ApiResponse<T> of(ResponseStatus responseStatus, T data) {
-        return ApiResponse.
-            <T>builder()
-            .time(Time.now())
-            .status(responseStatus.getStatus())
-            .message(responseStatus.getMessage())
-            .data(data)
-            .build();
+        return new ApiResponse<>(
+            responseStatus.getHttpStatusValue(),
+            responseStatus.getMessage(),
+            data
+        );
     }
 }

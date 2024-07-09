e380818909fa91d21b5f97e7edb228ea2ef8bcfb
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-10 05:04:25 +0900

    feat: TestController 수정
    - 기본 경로를 "/test"로 수정했습니다.
    - 예외 응답을 테스트하는 errorTest()를 생성했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/TestController.java
+++ BE/src/main/java/com/hymin/webtoon_review/TestController.java
@@ -1,20 +1,30 @@
 package com.hymin.webtoon_review;
 
+import com.hymin.webtoon_review.global.exception.GeneralException;
 import com.hymin.webtoon_review.global.response.ApiResponse;
+import com.hymin.webtoon_review.global.response.ResponseStatus;
+import com.hymin.webtoon_review.global.response.RestResponse;
 import com.hymin.webtoon_review.global.response.SliceResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("test")
-    public ApiResponse<String> test() {
+    @GetMapping
+    public RestResponse test() {
         return ApiResponse.onSuccess("test");
     }
 
-    @GetMapping("slice")
-    public ApiResponse<String> sliceTest() {
+    @GetMapping("/slice")
+    public RestResponse sliceTest() {
         return SliceResponse.onSuccess("slice test", false, 1, 0);
+    }
+
+    @GetMapping("/error")
+    public RestResponse errorTest() {
+        throw new GeneralException(ResponseStatus.BAD_REQUEST);
     }
 }

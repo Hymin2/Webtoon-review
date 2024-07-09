81782d2dc167c2c7e94ce5ed2d986976c978e2da
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-10 04:56:01 +0900

    feat: RestResponse 생성
    - 기본 응답 형식인 RestResponse를 생성했습니다.

---
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/RestResponse.java
@@ -0,0 +1,18 @@
+package com.hymin.webtoon_review.global.response;
+
+import com.hymin.webtoon_review.util.Time;
+import lombok.Getter;
+
+@Getter
+public class RestResponse {
+
+    private Integer status;
+    private String message;
+    private String time;
+
+    protected RestResponse(Integer status, String message) {
+        this.status = status;
+        this.message = message;
+        this.time = Time.now();
+    }
+}

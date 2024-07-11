f72193e65f586e4ca9595e1684abd95ae7498af0
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-12 04:45:08 +0900

    feat: UserService 생성
    - User와 관련한 처리를 하는 UserService를 생성했습니다.
    - 회원가입을 처리하는 register()를 생성했습니다.
    - 로그인을 처리하는 login()을 생성했습니다.

---
+++ BE/src/main/java/com/hymin/webtoon_review/user/service/UserService.java
@@ -0,0 +1,37 @@
+package com.hymin.webtoon_review.user.service;
+
+import com.hymin.webtoon_review.global.response.ResponseStatus;
+import com.hymin.webtoon_review.user.dto.UserRequest.LoginInfo;
+import com.hymin.webtoon_review.user.dto.UserRequest.RegisterInfo;
+import com.hymin.webtoon_review.user.dto.UserResponse.LoginResult;
+import com.hymin.webtoon_review.user.entity.User;
+import com.hymin.webtoon_review.user.exception.UserNotFoundException;
+import com.hymin.webtoon_review.user.mapper.UserMapper;
+import com.hymin.webtoon_review.user.repository.UserRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void register(RegisterInfo registerInfo) {
+        User user = UserMapper.toUser(registerInfo);
+
+        userRepository.save(user);
+    }
+
+    public LoginResult login(LoginInfo loginInfo) {
+        if (userRepository.existsByUsernameAndPassword(loginInfo.getUsername(),
+            loginInfo.getPassword())) {
+            throw new UserNotFoundException(ResponseStatus.USER_NOT_FOUND);
+        }
+
+        String sessionId = UUID.randomUUID().toString();
+
+        return UserMapper.toLoginResult(sessionId);
+    }
+}

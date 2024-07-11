ee0dec563e67fa145ef6266114af3444587fcaf3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-12 04:00:09 +0900

    feat: 응답 코드 추가
    - ResponseStatus에 404 User Not Found를 추가했습니다.
    - 해당 코드는 사용자를 찾을 수 없을 때 발생하는 예외입니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
@@ -11,6 +11,8 @@ public enum ResponseStatus {
     OK(HttpStatus.OK, "정상 처리 됐습니다."),
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;

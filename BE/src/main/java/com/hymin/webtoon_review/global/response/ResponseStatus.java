2bb8889f96de5813441aea43d774b0928d7a77fe
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-12 04:40:42 +0900

    feat: Created 응답 코드 추가
    - 서버에 정상적으로 저장됨을 알리는 Created 응답 코드를 추가했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ResponseStatus {
     OK(HttpStatus.OK, "정상 처리 됐습니다."),
+    CREATED(HttpStatus.CREATED, "정상 처리 됐습니다."),
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
 

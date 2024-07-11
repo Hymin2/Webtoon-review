be2129ebb08be3a8d17ef92ee75d62c585a36534
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-12 04:16:02 +0900

    feat: UserMapper 생성
    - User와 관련한 Entity와 Dto를 변환해주는 Mapping class입니다.
    - RegisterInfo -> User 로 변환해주는 toUser()를 생성했습니다.
    - Long -> RegisterResult 로 변환해주는 toRegisterResult()를 생성했습니다.
    - String -> LoginResult 로 변환해주는 toLoginResult()를 생성했습니다.

---
+++ BE/src/main/java/com/hymin/webtoon_review/user/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.hymin.webtoon_review.user.mapper;
+
+import com.hymin.webtoon_review.user.dto.UserRequest.RegisterInfo;
+import com.hymin.webtoon_review.user.dto.UserResponse.LoginResult;
+import com.hymin.webtoon_review.user.dto.UserResponse.RegisterResult;
+import com.hymin.webtoon_review.user.entity.User;
+
+public class UserMapper {
+
+    public static User toUser(RegisterInfo registerInfo) {
+        return User.builder()
+            .username(registerInfo.getUsername())
+            .password(registerInfo.getPassword())
+            .nickname(registerInfo.getNickname())
+            .build();
+    }
+
+    public static RegisterResult toRegisterResult(Long id) {
+        return RegisterResult.builder()
+            .id(id)
+            .build();
+    }
+
+    public static LoginResult toLoginResult(String sessionId) {
+        return LoginResult.builder()
+            .sessionId(sessionId)
+            .build();
+    }
+}

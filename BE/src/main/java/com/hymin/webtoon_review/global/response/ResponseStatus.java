88ced6c9dd71ee65b7c25103bc1a3ebb1074f048
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-15 22:58:50 +0900

    feat: AlreadyUserExists status 추가
    - 이미 존재하는 사용자에 대한 생성 요청이 올 경우 발생하는 상태 코드입니다.
    - 409 Conflict가 발생합니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/response/ResponseStatus.java
@@ -15,7 +15,8 @@ public enum ResponseStatus {
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    ;
+
+    ALREADY_USER_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

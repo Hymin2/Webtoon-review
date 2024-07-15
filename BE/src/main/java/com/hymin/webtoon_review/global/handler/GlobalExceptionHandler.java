fec5ae0d66250f4d8ad285aef971ccade0025cea
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-15 22:41:19 +0900

    feat: MethodArgumentNotValid 예외 핸들링 추가
    - GlobalExceptionHandler에서 @Valid가 붙은 파라미터에 대한 유효성 검사가 실패할 때 발생하는 예외인 MethodArgumentNotValid을 핸들링할 수 있도록 추가했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/global/handler/GlobalExceptionHandler.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/handler/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.hymin.webtoon_review.global.handler;
 
 import com.hymin.webtoon_review.global.exception.GeneralException;
 import com.hymin.webtoon_review.global.response.ErrorResponse;
+import com.hymin.webtoon_review.global.response.ResponseStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -15,8 +17,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+        HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return createResponseEntity(ex, ErrorResponse.of(ResponseStatus.INVALID_INPUT), headers,
+            status,
+            request);
+    }
+
     @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<?> handleGeneralException(final GeneralException e,
+    public ResponseEntity<Object> handleGeneralException(final GeneralException e,
         final WebRequest request) {
         return createResponseEntity(e,
             ErrorResponse.of(e.getResponseStatus()),
@@ -26,7 +36,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
     }
 
-    private ResponseEntity<?> createResponseEntity(Exception e, Object body, HttpHeaders headers,
+    private ResponseEntity<Object> createResponseEntity(Exception e, Object body,
+        HttpHeaders headers,
         HttpStatusCode httpStatusCode, WebRequest request) {
         return super.handleExceptionInternal(
             e,

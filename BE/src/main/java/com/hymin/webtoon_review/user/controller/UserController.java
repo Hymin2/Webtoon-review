3f35ff230313998ffa7d46e193899c8c7c4b1e51
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-16 02:43:30 +0900

    feat: ID와 닉네임 중복 확인 API 추가

--- BE/src/main/java/com/hymin/webtoon_review/user/controller/UserController.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/controller/UserController.java
@@ -7,9 +7,11 @@ import com.hymin.webtoon_review.user.dto.UserRequest.RegisterInfo;
 import com.hymin.webtoon_review.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +31,15 @@ public class UserController {
     @PostMapping("/login")
     public RestResponse login(@Valid @RequestBody LoginInfo loginInfo) {
         return ApiResponse.onSuccess(userService.login(loginInfo));
+    }
+
+    @GetMapping("/check/username")
+    public RestResponse checkDuplicatedUsername(@RequestParam String username) {
+        return ApiResponse.onSuccess(userService.checkDuplicatedUsername(username));
+    }
+
+    @GetMapping("/check/nickname")
+    public RestResponse checkDuplicatedNickname(@RequestParam String nickname) {
+        return ApiResponse.onSuccess(userService.checkDuplicatedNickname(nickname));
     }
 }

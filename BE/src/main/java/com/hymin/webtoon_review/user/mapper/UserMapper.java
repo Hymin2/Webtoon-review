09c016a1605603d57f538fe358c8be5ed3fd8563
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-18 07:49:10 +0900

    feat: UserMapper 수정
    - User 객체를 생성할 때 암호화된 비밀번호로 생성될 수 있도록 수정했습니다.
    - toLoginResult()를 제거했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/user/mapper/UserMapper.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/mapper/UserMapper.java
@@ -1,22 +1,15 @@
 package com.hymin.webtoon_review.user.mapper;
 
 import com.hymin.webtoon_review.user.dto.UserRequest.RegisterInfo;
-import com.hymin.webtoon_review.user.dto.UserResponse.LoginResult;
 import com.hymin.webtoon_review.user.entity.User;
 
 public class UserMapper {
 
-    public static User toUser(RegisterInfo registerInfo) {
+    public static User toUser(RegisterInfo registerInfo, String encodedPassword) {
         return User.builder()
             .username(registerInfo.getUsername())
-            .password(registerInfo.getPassword())
+            .password(encodedPassword)
             .nickname(registerInfo.getNickname())
-            .build();
-    }
-
-    public static LoginResult toLoginResult(String sessionId) {
-        return LoginResult.builder()
-            .sessionId(sessionId)
             .build();
     }
 }

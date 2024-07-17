72585177ea51e3460e03fb8b09a50889a620495a
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-18 07:46:35 +0900

    feat: UserRepository 수정
    - username을 통해 User 객체를 가져오는 함수 findByUsername()을 추가했습니다.
    - username과 password에 대한 User가 존재 여부를 확인하는 함수 existsByUsernameAndPassword()를 제거했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/user/repository/UserRepository.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.hymin.webtoon_review.user.repository;
 
 import com.hymin.webtoon_review.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Boolean existsByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsername(String username);
 
     Boolean existsByUsernameOrNickname(String username, String nickname);
 

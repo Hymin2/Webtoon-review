277bbe9f3745c5612c490476b37e2361cedc09c8
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-18 06:40:04 +0900

    feat: UserService의 회원가입 및 로그인 기능 수정
    - 회원가입 시 비밀번호가 암호화되어 저장되도록 하였습니다.
    - 회원가입 시 USER 권한이 추가되도록 변경했습니다.
    - 로그인 기존 코드를 삭제하였습니다.
    - JWT 토큰을 발급할 수 있도록 로그인 기능을 수정할 예정입니다.

--- BE/src/main/java/com/hymin/webtoon_review/user/service/UserService.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/service/UserService.java
@@ -1,44 +1,48 @@
 package com.hymin.webtoon_review.user.service;
 
 import com.hymin.webtoon_review.global.response.ResponseStatus;
-import com.hymin.webtoon_review.user.dto.UserRequest.LoginInfo;
 import com.hymin.webtoon_review.user.dto.UserRequest.RegisterInfo;
 import com.hymin.webtoon_review.user.dto.UserResponse.LoginResult;
+import com.hymin.webtoon_review.user.entity.Authority;
 import com.hymin.webtoon_review.user.entity.User;
 import com.hymin.webtoon_review.user.exception.AlreadyUserExistsException;
-import com.hymin.webtoon_review.user.exception.UserNotFoundException;
 import com.hymin.webtoon_review.user.mapper.UserMapper;
+import com.hymin.webtoon_review.user.repository.AuthorityRepository;
 import com.hymin.webtoon_review.user.repository.UserRepository;
-import java.util.UUID;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthorityRepository authorityRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void register(RegisterInfo registerInfo) {
         if (userRepository.existsByUsernameOrNickname(registerInfo.getUsername(),
             registerInfo.getNickname())) {
             throw new AlreadyUserExistsException(ResponseStatus.ALREADY_USER_EXISTS);
         }
 
-        User user = UserMapper.toUser(registerInfo);
-
+        String encodedPassword = passwordEncoder.encode(registerInfo.getPassword());
+        User user = UserMapper.toUser(registerInfo, encodedPassword);
         userRepository.save(user);
+
+        Authority authority = createUserAuthority(user);
+        user.setAuthorities(List.of(authority));
     }
 
-    public LoginResult login(LoginInfo loginInfo) {
-        if (!userRepository.existsByUsernameAndPassword(loginInfo.getUsername(),
-            loginInfo.getPassword())) {
-            throw new UserNotFoundException(ResponseStatus.USER_NOT_FOUND);
-        }
+    public LoginResult login(Authentication authentication) {
+        // TODO Jwt 생성 후 리턴
 
-        String sessionId = UUID.randomUUID().toString();
-
-        return UserMapper.toLoginResult(sessionId);
+        return null;
     }
 
     public Boolean checkDuplicatedUsername(String username) {
@@ -47,5 +51,16 @@ public class UserService {
 
     public Boolean checkDuplicatedNickname(String nickname) {
         return !userRepository.existsByNickname(nickname);
+    }
+
+    private Authority createUserAuthority(User user) {
+        Authority authority = Authority.builder()
+            .name("ROLE_USER")
+            .user(user)
+            .build();
+
+        authorityRepository.save(authority);
+
+        return authority;
     }
 }

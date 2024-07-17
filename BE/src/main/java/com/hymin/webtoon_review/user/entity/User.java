f3cf579688bed3c5993f953ae655f9b94e207abd
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-18 06:41:54 +0900

    feat: User에 Authority와 OneToMany 관계 설정

--- BE/src/main/java/com/hymin/webtoon_review/user/entity/User.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/entity/User.java
@@ -1,11 +1,15 @@
 package com.hymin.webtoon_review.user.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,4 +35,11 @@ public class User {
 
     @Column(nullable = false)
     private String nickname;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Authority> authorities;
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
 }

378166b85b0f47f4c00b082e1de8dc46f6dcbfc6
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-18 06:40:40 +0900

    feat: Authority 생성
    - User의 권한을 저장하는 엔티티(테이블)입니다.

---
+++ BE/src/main/java/com/hymin/webtoon_review/user/entity/Authority.java
@@ -0,0 +1,34 @@
+package com.hymin.webtoon_review.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private User user;
+}

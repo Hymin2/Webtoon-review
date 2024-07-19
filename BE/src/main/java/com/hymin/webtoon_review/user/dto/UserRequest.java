9aeb8cf1772c70349eef38071b237466b16f57f7
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-20 04:22:50 +0900

    refactor: UserRequest 수정
    - RegisterInfo에 빌더를 제거했습니다.
    - LoginInfo에 유효성을 제거했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/user/dto/UserRequest.java
+++ BE/src/main/java/com/hymin/webtoon_review/user/dto/UserRequest.java
@@ -3,14 +3,12 @@ package com.hymin.webtoon_review.user.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class UserRequest {
 
     @Getter
-    @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class RegisterInfo {
@@ -29,17 +27,11 @@ public class UserRequest {
     }
 
     @Getter
-    @Builder
     @NoArgsConstructor
     @AllArgsConstructor
     public static class LoginInfo {
 
-        @NotBlank
-        @Pattern(regexp = "^[a-zA-Z0-9\\-_]{4,20}$")
         private String username;
-
-        @NotBlank
-        @Pattern(regexp = "^[a-zA-Z0-9~․!@#$%^&*()_\\-+=\\[\\]\\|\\\\\\;:‘“<>,.\\/?]{4,20}$")
         private String password;
     }
 }

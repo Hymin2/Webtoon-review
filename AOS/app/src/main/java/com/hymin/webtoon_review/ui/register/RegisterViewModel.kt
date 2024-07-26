08cd233ae8a38e67a8d9fadf9d4bfde9f9340f8e
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-27 07:59:48 +0900

    feat: RegisterViewModel 생성 및 구현
    - 아이디와 닉네임의 중복 확인 요청을 통해 isDuplicatedUsername, isDuplicatedNickname 값을 변경합니다.
    - 회원가입 버튼을 클릭한 경우
      - 입력값이 비어있으면 종료합니다.
      - 중복된 값이 있으면 종료합니다.
      - 그 외에 회원가입 요청을 보낸 후 _isRegisterSuccess 값을 변경합니다.

---
+++ AOS/app/src/main/java/com/hymin/webtoon_review/ui/register/RegisterViewModel.kt
@@ -0,0 +1,91 @@
+package com.hymin.webtoon_review.ui.register
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hymin.webtoon_review.data.dto.request.user.RegisterRequest
+import com.hymin.webtoon_review.data.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel() {
+
+    var username = MutableLiveData("")
+    var password = MutableLiveData("")
+    var nickname = MutableLiveData("")
+
+    private val _isUsernameEmpty = MutableLiveData<Boolean>()
+    val isUsernameEmpty: LiveData<Boolean> get() = _isUsernameEmpty
+
+    private val _isPasswordEmpty = MutableLiveData<Boolean>()
+    val isPasswordEmpty: LiveData<Boolean> get() = _isPasswordEmpty
+
+    private val _isNicknameEmpty = MutableLiveData<Boolean>()
+    val isNicknameEmpty: LiveData<Boolean> get() = _isNicknameEmpty
+
+    private val _isDuplicatedUsername = MutableLiveData<Boolean>()
+    val isDuplicatedUsername: LiveData<Boolean> get() = _isDuplicatedUsername
+
+    private val _isDuplicatedNickname = MutableLiveData<Boolean>()
+    val isDuplicatedNickname: LiveData<Boolean> get() = _isDuplicatedNickname
+
+    private val _isRegisterSuccess = MutableLiveData<Boolean>()
+    val isRegisterSuccess: LiveData<Boolean> get() = _isRegisterSuccess
+
+    fun onRegisterClicked() {
+        _isUsernameEmpty.value = username.value.isNullOrEmpty()
+        _isPasswordEmpty.value = password.value.isNullOrEmpty()
+        _isNicknameEmpty.value = nickname.value.isNullOrEmpty()
+
+        if (_isUsernameEmpty.value!! || _isPasswordEmpty.value!! || _isNicknameEmpty.value!! || _isDuplicatedUsername.value!! || _isDuplicatedNickname.value!!) {
+            return
+        }
+
+        viewModelScope.launch {
+            _isRegisterSuccess.value = userRepository.register(
+                RegisterRequest(
+                    username.value!!,
+                    password.value!!,
+                    nickname.value!!
+                )
+            ).isSuccessful
+        }
+    }
+
+    fun onUsernameSubmit() {
+        _isUsernameEmpty.value = username.value.isNullOrEmpty()
+
+        if (_isUsernameEmpty.value!!) {
+            return
+        }
+
+        viewModelScope.launch {
+            val response = userRepository.checkDuplicatedUsername(username.value!!)
+
+            if (response.isSuccessful) {
+                _isDuplicatedUsername.value = response.body()!!.data
+            }
+        }
+    }
+
+    fun onNicknameSubmit() {
+        _isDuplicatedNickname.value = nickname.value.isNullOrEmpty()
+
+        if (_isDuplicatedNickname.value!!) {
+            return
+        }
+
+        viewModelScope.launch {
+            val response = userRepository.checkDuplicatedNickname(nickname.value!!)
+
+            if (response.isSuccessful) {
+                _isDuplicatedNickname.value = response.body()!!.data
+            }
+        }
+    }
+}

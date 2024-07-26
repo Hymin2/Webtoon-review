ec159b96665c55c80378721e065bcf9a101ec0cc
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-07-27 08:10:27 +0900

    feat: OkHttp 타임아웃 설정

--- AOS/app/src/main/java/com/hymin/webtoon_review/di/NetworkModule.kt
+++ AOS/app/src/main/java/com/hymin/webtoon_review/di/NetworkModule.kt
@@ -10,6 +10,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -22,7 +23,11 @@ class NetworkModule {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
-            }).build()
+            })
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .build()
     }
 
     @Provides

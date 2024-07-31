284cb0fa1ec55158f341390a836d01f170251c02
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 06:21:00 +0900

    feat: WebtoonService 생성 및 웹툰 리스트 조회 함수 구현

---
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/service/WebtoonService.java
@@ -0,0 +1,20 @@
+package com.hymin.webtoon_review.webtoon.service;
+
+import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonInfo;
+import com.hymin.webtoon_review.webtoon.repository.WebtoonRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebtoonService {
+
+    private final WebtoonRepository webtoonRepository;
+
+    public List<WebtoonInfo> getWentoons(Pageable pageable, List<String> dayOfWeek,
+        List<String> platform, List<String> genre) {
+        return webtoonRepository.getWebtoons(pageable, dayOfWeek, platform, genre);
+    }
+}

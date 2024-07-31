77a04ece627ee8d6acbfaeab7fb190866c651edb
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 06:12:41 +0900

    feat: EnableJpaAuditing 추가

--- BE/src/main/java/com/hymin/webtoon_review/WebtoonReviewApplication.java
+++ BE/src/main/java/com/hymin/webtoon_review/WebtoonReviewApplication.java
@@ -2,12 +2,14 @@ package com.hymin.webtoon_review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WebtoonReviewApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebtoonReviewApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebtoonReviewApplication.class, args);
+    }
 
 }

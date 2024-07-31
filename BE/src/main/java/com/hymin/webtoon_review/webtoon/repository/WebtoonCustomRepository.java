23f080cbfc773c3274cdc17f4089068c1d414351
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 06:21:35 +0900

    feat: querydsl을 사용할 repository인 WebtoonCustomRepository 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/WebtoonCustomRepository.java
@@ -0,0 +1,11 @@
+package com.hymin.webtoon_review.webtoon.repository;
+
+import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonInfo;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+public interface WebtoonCustomRepository {
+
+    List<WebtoonInfo> getWebtoons(Pageable pageable, List<String> dayOfWeek, List<String> platform,
+        List<String> genre);
+}

5c4baaf15556a14bf41b3f0d8182bcef2f879d1e
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 06:17:03 +0900

    feat: DayOfWeek enum 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/enums/DayOfWeek.java
@@ -0,0 +1,9 @@
+package com.hymin.webtoon_review.webtoon.entity.enums;
+
+public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+    public String toString() {
+        return this.name();
+    }
+}

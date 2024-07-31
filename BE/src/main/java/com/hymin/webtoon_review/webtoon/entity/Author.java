712bed8e307f0e7dedb0b3584db476acf3fe18af
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 06:16:38 +0900

    feat: Author entity 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/Author.java
@@ -0,0 +1,33 @@
+package com.hymin.webtoon_review.webtoon.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "author")
+    private List<WebtoonAuthor> webtoonAuthors;
+}

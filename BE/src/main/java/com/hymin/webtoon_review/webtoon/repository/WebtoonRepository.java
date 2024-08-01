b1d1e6aca94d9c7caab347ae89b244a97a32234d
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-01 22:12:20 +0900

    feat: WebtoonRepository에 @Repository 추가하여 Bean 등록

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/WebtoonRepository.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/WebtoonRepository.java
@@ -2,7 +2,9 @@ package com.hymin.webtoon_review.webtoon.repository;
 
 import com.hymin.webtoon_review.webtoon.entity.Webtoon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long>, WebtoonCustomRepository {
 
 }

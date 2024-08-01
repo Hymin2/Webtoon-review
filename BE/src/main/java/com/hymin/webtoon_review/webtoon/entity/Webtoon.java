3a3e16d4f1faf60bb0268122a386993994c6ba52
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-02 03:46:28 +0900

    feat: Webtoon의 platform에 fetch를 LAZY로 변경

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/Webtoon.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/Webtoon.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,8 +45,8 @@ public class Webtoon extends BaseEntity {
     @Enumerated(value = EnumType.STRING)
     private Status status;
 
-    @ManyToOne
     @JoinColumn(name = "platform_id")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Platform platform;
 
     @OneToMany(mappedBy = "webtoon")

c639b5e9520920f75fc39914c289d118bf7ee2b8
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-02 03:55:03 +0900

    refactor: WebtoonService의 웹툰 리스트 조회 함수 수정

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/service/WebtoonService.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/service/WebtoonService.java
@@ -1,6 +1,12 @@
 package com.hymin.webtoon_review.webtoon.service;
 
 import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonInfo;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.Author;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.DayOfWeek;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.Genre;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.Platform;
+import com.hymin.webtoon_review.webtoon.entity.Webtoon;
+import com.hymin.webtoon_review.webtoon.mapper.WebtoonMapper;
 import com.hymin.webtoon_review.webtoon.repository.WebtoonRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +19,22 @@ public class WebtoonService {
 
     private final WebtoonRepository webtoonRepository;
 
-    public List<WebtoonInfo> getWentoons(Pageable pageable, String name, List<String> dayOfWeek,
-        List<String> platform, List<String> genre) {
-        return webtoonRepository.getWebtoons(pageable, name, dayOfWeek, platform, genre);
+    public List<WebtoonInfo> getWentoons(Pageable pageable, String name, List<String> daysOfWeek,
+        List<String> platforms, List<String> genres) {
+        List<Webtoon> webtoons = webtoonRepository.getWebtoons(pageable, name, daysOfWeek,
+            platforms, genres);
+
+        List<Long> webtoonIdList = webtoons
+            .stream()
+            .map(Webtoon::getId)
+            .toList();
+
+        List<Platform> platformList = webtoonRepository.getPlatforms(webtoonIdList);
+        List<DayOfWeek> dayOfWeekList = webtoonRepository.getDayOfWeek(webtoonIdList);
+        List<Genre> genreList = webtoonRepository.getGenres(webtoonIdList);
+        List<Author> authorList = webtoonRepository.getAuthors(webtoonIdList);
+
+        return WebtoonMapper.toWebtoonList(webtoons, platformList, dayOfWeekList, genreList,
+            authorList);
     }
 }

90cd729bf6c37407d0a50eba42f9366e84a88cc7
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-08-09 06:04:07 +0900

    feat: WebtoonController에서 웹툰 조회 함수에 Authentication 객체 주입

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/controller/WebtoonController.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/controller/WebtoonController.java
@@ -1,5 +1,6 @@
 package com.hymin.webtoon_review.webtoon.controller;
 
+import com.hymin.webtoon_review.global.annotation.Auth;
 import com.hymin.webtoon_review.global.response.RestResponse;
 import com.hymin.webtoon_review.global.response.SliceResponse;
 import com.hymin.webtoon_review.webtoon.service.WebtoonService;
@@ -7,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +23,14 @@ public class WebtoonController {
 
     @GetMapping
     public RestResponse getWebtoons(
-        @PageableDefault(size = 10, sort = "updateAt,desc") Pageable pageable,
+        @Auth Authentication authentication,
+        @PageableDefault(size = 10, sort = "updatedAt,desc") Pageable pageable,
         @RequestParam(name = "name", required = false) String name,
         @RequestParam(name = "dayOfWeek", required = false) List<String> dayOfWeek,
         @RequestParam(name = "platform", required = false) List<String> platform,
         @RequestParam(name = "genre", required = false) List<String> genre) {
         return SliceResponse.onSuccess(
-            webtoonService.getWentoons(pageable, name, dayOfWeek, platform, genre),
+            webtoonService.getWentoons(authentication, pageable, name, dayOfWeek, platform, genre),
             pageable.getPageSize());
     }
 }

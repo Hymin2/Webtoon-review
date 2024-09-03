40e4163ef420faeedbe0d310f2d2ef1cd724e2f5
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-09-03 21:45:54 +0900

    fix: ReplyMapper에서 status를 저장하지 않는 현상 수정

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/mapper/ReplyMapper.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/mapper/ReplyMapper.java
@@ -1,15 +1,18 @@
 package com.hymin.webtoon_review.webtoon.mapper;
 
 import com.hymin.webtoon_review.user.entity.User;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonRequest.ReplyInfo;
 import com.hymin.webtoon_review.webtoon.entity.Comment;
 import com.hymin.webtoon_review.webtoon.entity.Reply;
 
 public class ReplyMapper {
 
-    public static Reply toReply(User user, Comment comment) {
+    public static Reply toReply(User user, Comment comment, ReplyInfo replyInfo) {
         return Reply.builder()
             .user(user)
             .comment(comment)
+            .content(replyInfo.getContent())
+            .status(Boolean.TRUE)
             .build();
     }
 }

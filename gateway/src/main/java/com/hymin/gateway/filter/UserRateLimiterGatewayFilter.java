5291fcd36557bfe57f33270ff28c6be1f6e3d1d3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-09-25 22:05:33 +0900

    refactor: 사용자 처리율 제한기 리팩토링

--- gateway/src/main/java/com/hymin/gateway/filter/UserRateLimiterGatewayFilter.java
+++ gateway/src/main/java/com/hymin/gateway/filter/UserRateLimiterGatewayFilter.java
@@ -2,23 +2,19 @@ package com.hymin.gateway.filter;
 
 import com.hymin.gateway.redis.RedisSortedSetService;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
 public class UserRateLimiterGatewayFilter extends
-    AbstractGatewayFilterFactory<UserRateLimiterGatewayFilter.Config> {
+    RateLimiterGatewayFilter<UserRateLimiterGatewayFilter.Config> {
 
-    private final RedisSortedSetService redisSortedSetService;
-
+    public UserRateLimiterGatewayFilter(Class<Config> configClass,
+        RedisSortedSetService redisSortedSetService) {
+        super(configClass, redisSortedSetService);
+    }
+    
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
@@ -26,34 +22,12 @@ public class UserRateLimiterGatewayFilter extends
             String request = config.getRequest();
 
             if (isAllowed(username, request, config.getRequestLimitPerInterval())) {
-
-                return redisSortedSetService
-                    .add(username, request, System.nanoTime())
-                    .then(redisSortedSetService.setExpire(username, request,
-                        config.getRateLimitIntervalInSeconds()))
-                    .then(chain.filter(exchange));
+                return onSuccess(exchange, chain, username, request,
+                    config.getRequestLimitPerInterval());
             } else {
                 return onError(exchange);
             }
         };
-    }
-
-    private boolean isAllowed(String username, String request, Integer requestLimitPerInterval) {
-        return Boolean.TRUE.equals(redisSortedSetService.size(username, request)
-            .flatMap(size -> {
-                if (size != null && size < requestLimitPerInterval) {
-                    return Mono.just(true);
-                } else {
-                    return Mono.just(false);
-                }
-            }).block());
-    }
-
-    private Mono<Void> onError(ServerWebExchange exchange) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
-
-        return response.setComplete();
     }
 
     @Getter

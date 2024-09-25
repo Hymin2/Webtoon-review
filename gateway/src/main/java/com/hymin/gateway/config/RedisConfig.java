6d8e68df7c55b53a14549fe53d4997e2dc9f65d8
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-09-25 22:01:42 +0900

    fix: ReactiveRedisConnectionFactory 빈 등록 오류 및 ReactiveRedisOperations 생성 오류 수정

--- gateway/src/main/java/com/hymin/gateway/config/RedisConfig.java
+++ gateway/src/main/java/com/hymin/gateway/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.hymin.gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
@@ -8,10 +11,13 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class,
+    RedisReactiveAutoConfiguration.class})
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -33,12 +39,17 @@ public class RedisConfig {
     }
 
     @Bean
-    public ReactiveRedisOperations<String, Object> reactiveRedisOperations(
-        ReactiveRedisConnectionFactory connectionFactory) {
+    public ReactiveRedisOperations<String, Object> reactiveRedisOperations() {
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(
+            Object.class);
+
         return new ReactiveRedisTemplate<>(
-            connectionFactory,
-            RedisSerializationContext.<String, Object>newSerializationContext()
-                .value(new GenericJackson2JsonRedisSerializer())
+            redisConnectionFactory(),
+            RedisSerializationContext.<String, Object>newSerializationContext(
+                    new StringRedisSerializer())
+                .value(serializer)
+                .hashKey(serializer)
+                .hashValue(serializer)
                 .build()
         );
     }

b8ce94978b3bc38f014148b7a73c6c8b19b1c9ba
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-09-25 20:04:11 +0900

    feat: config 서버 생성

---
+++ config/src/test/java/com/hymin/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hymin.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

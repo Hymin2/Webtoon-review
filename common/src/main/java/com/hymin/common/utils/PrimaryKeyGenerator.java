4346a5836e4f529899112fe2c8d0f5a747af5f46
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2024-12-12 20:55:37 +0900

    remove: 다른 서버 삭제

--- common/src/main/java/com/hymin/common/utils/PrimaryKeyGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.hymin.common.utils;
-
-public interface PrimaryKeyGenerator {
-
-    long generate();
-}
--- user/src/main/java/com/hymin/user/config/GlobalHandlerException.java
+++
@@ -1,9 +0,0 @@
-package com.hymin.user.config;
-
-import com.hymin.common.handler.GlobalExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalHandlerException extends GlobalExceptionHandler {
-
-}

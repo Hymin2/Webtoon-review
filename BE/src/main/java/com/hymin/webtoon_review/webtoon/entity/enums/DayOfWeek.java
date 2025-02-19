aecb0068ba5fd5ff818b4ccc229990e047126ce2
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 06:37:19 +0900

    refactor: 요일 enum의 이름 수정

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/enums/DayOfWeek.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/entity/enums/DayOfWeek.java
@@ -1,7 +1,7 @@
 package com.hymin.webtoon_review.webtoon.entity.enums;
 
 public enum DayOfWeek {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    월, 화, 수, 목, 금, 토, 일;
 
     public String toString() {
         return this.name();

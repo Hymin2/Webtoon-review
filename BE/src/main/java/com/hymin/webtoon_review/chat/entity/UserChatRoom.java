001320a3df373e10ed91d08814305561b912cd20
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:12:53 +0900

    feat: UserChatRoom entity 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/entity/UserChatRoom.java
@@ -0,0 +1,39 @@
+package com.hymin.webtoon_review.chat.entity;
+
+import com.hymin.webtoon_review.global.BaseEntity;
+import com.hymin.webtoon_review.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserChatRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long lastReadMessageId;
+    private Boolean isConnected;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+}

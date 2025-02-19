036809239e92a0b791389d6fd5065cfa673463f3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:12:07 +0900

    feat: 채팅 관련 예외 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/exception/InvalidChatRoomAccessException.java
@@ -0,0 +1,11 @@
+package com.hymin.webtoon_review.chat.exception;
+
+import com.hymin.webtoon_review.global.exception.GeneralException;
+import com.hymin.webtoon_review.global.response.ResponseStatus;
+
+public class InvalidChatRoomAccessException extends GeneralException {
+
+    public InvalidChatRoomAccessException() {
+        super(ResponseStatus.INVALID_CHAT_ROOM_ACCESS);
+    }
+}

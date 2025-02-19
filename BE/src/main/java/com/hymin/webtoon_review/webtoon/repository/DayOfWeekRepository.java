31c8d576ba942d6412dc902a3379c60d98a0d4d1
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 06:50:45 +0900

    feat: 웹툰의 모든 요일 조회 함수 추가

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/DayOfWeekRepository.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/DayOfWeekRepository.java
@@ -1,10 +1,15 @@
 package com.hymin.webtoon_review.webtoon.repository;
 
 import com.hymin.webtoon_review.webtoon.entity.DayOfWeek;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DayOfWeekRepository extends JpaRepository<DayOfWeek, Long> {
 
+    @Query(value = "select dow from DayOfWeek dow inner join WebtoonDayOfWeek wdow on wdow.dayOfWeek = dow inner join Webtoon w on wdow.webtoon = w where w.id =:id")
+    List<DayOfWeek> findByWebtoonId(@Param("id") Long id);
 }

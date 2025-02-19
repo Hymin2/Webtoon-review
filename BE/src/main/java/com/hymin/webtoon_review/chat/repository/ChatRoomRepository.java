3261a61cf4ce66aae7c48ecb448f6bd03608e4ca
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:14:15 +0900

    feat: ChatRoomRepository 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.hymin.webtoon_review.chat.repository;
+
+import com.hymin.webtoon_review.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}

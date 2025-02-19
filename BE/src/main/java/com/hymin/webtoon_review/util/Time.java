47553130ba71fdc5b08d7800e5611e4a271b9542
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 06:34:58 +0900

    feat: Time.now()의 밀리초 단위까지 추가

--- BE/src/main/java/com/hymin/webtoon_review/util/Time.java
+++ BE/src/main/java/com/hymin/webtoon_review/util/Time.java
@@ -6,6 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class Time {
 
     public static String now() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
     }
 }

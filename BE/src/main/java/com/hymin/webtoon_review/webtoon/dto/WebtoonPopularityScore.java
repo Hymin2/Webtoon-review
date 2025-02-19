3f31aa3bbfb97fc21568cfa442f77666baca02ab
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:05:41 +0900

    feat: 웹툰의 인기 점수 Job의 파라미터로 사용될 WebtoonPopularityScore 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/dto/WebtoonPopularityScore.java
@@ -0,0 +1,16 @@
+package com.hymin.webtoon_review.webtoon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebtoonPopularityScore {
+
+    private Long id;
+    private Integer score;
+}

4f3bba4eca55e07a3255142745950ad301fd8a25
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:13:09 +0900

    feat: NotificationService 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.hymin.webtoon_review.chat.service;
+
+import com.hymin.webtoon_review.chat.dto.ChatRequest.ChatMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    public void send(ChatMessage chatMessage) {
+
+    }
+}

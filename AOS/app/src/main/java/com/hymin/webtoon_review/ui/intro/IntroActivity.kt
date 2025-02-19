a6f27960e63a4ef790e20719768cd297d9401128
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 05:30:00 +0900

    feat: 앱의 첫 화면인 IntroActivity 생성

--- AOS/app/src/main/java/com/hymin/webtoon_review/ui/intro/IntroActivity.kt
+++ AOS/app/src/main/java/com/hymin/webtoon_review/ui/intro/IntroActivity.kt
@@ -1,19 +1,36 @@
 package com.hymin.webtoon_review.ui.intro
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.hymin.webtoon_review.databinding.ActivityIntroBinding
+import com.hymin.webtoon_review.ui.login.LoginActivity
+import com.hymin.webtoon_review.ui.main.MainActivity
+import com.hymin.webtoon_review.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.isExistJwt().observe(this, Observer { exist ->
+            if (exist) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        })
     }
 }

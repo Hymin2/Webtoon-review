a95b9ad7a96522ba166aa41dff84c359eaa6311e
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 06:50:52 +0900

    feat: 웹툰의 모든 장르 조회 함수 추가

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/GenreRepository.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/GenreRepository.java
@@ -1,10 +1,15 @@
 package com.hymin.webtoon_review.webtoon.repository;
 
 import com.hymin.webtoon_review.webtoon.entity.Genre;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
+    @Query(value = "select g from Genre g inner join WebtoonGenre wg on wg.genre = g inner join Webtoon w on wg.webtoon = w where w.id =:id")
+    List<Genre> findByWebtoonId(@Param("id") Long id);
 }

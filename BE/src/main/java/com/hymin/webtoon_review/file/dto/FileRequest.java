6951d03147c266c6601c3b6bcc067baf4fb1ead2
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:10:38 +0900

    feat: FileUpload dto 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/file/dto/FileRequest.java
@@ -0,0 +1,19 @@
+package com.hymin.webtoon_review.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class FileRequest {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FileUploadInfo {
+
+        private Long userId;
+        private String type;
+        private String fileName;
+        private Integer chunkSize;
+    }
+}

178afcd606ccc52ed131703d076e4a13129554c6
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-20 07:06:59 +0900

    feat: WebtoonFacade 수정 및 웹툰 관렴 함수 추가

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/facade/WebtoonFacade.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/facade/WebtoonFacade.java
@@ -1,266 +1,132 @@
 package com.hymin.webtoon_review.webtoon.facade;
 
-import com.hymin.webtoon_review.global.exception.GeneralException;
+import com.hymin.webtoon_review.global.async.Job;
+import com.hymin.webtoon_review.global.async.JobQueue;
+import com.hymin.webtoon_review.global.async.TopicNames;
 import com.hymin.webtoon_review.global.response.ResponseStatus;
-import com.hymin.webtoon_review.user.entity.Bookmark;
 import com.hymin.webtoon_review.user.entity.User;
 import com.hymin.webtoon_review.user.entity.WebtoonRecommend;
 import com.hymin.webtoon_review.user.service.UserService;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonRequest.CommentInfo;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonRequest.ReplyInfo;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonInfo;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.AuthorSelectResult;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.DayOfWeekSelectResult;
-import com.hymin.webtoon_review.webtoon.dto.WebtoonSelectResult.GenreSelectResult;
-import com.hymin.webtoon_review.webtoon.entity.Comment;
-import com.hymin.webtoon_review.webtoon.entity.CommentRecommend;
-import com.hymin.webtoon_review.webtoon.entity.Reply;
-import com.hymin.webtoon_review.webtoon.entity.ReplyRecommend;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.Category;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonDetails;
+import com.hymin.webtoon_review.webtoon.dto.WebtoonResponse.WebtoonSimple;
 import com.hymin.webtoon_review.webtoon.entity.Webtoon;
-import com.hymin.webtoon_review.webtoon.mapper.BookmarkMapper;
-import com.hymin.webtoon_review.webtoon.mapper.CommentMapper;
-import com.hymin.webtoon_review.webtoon.mapper.CommentRecommendMapper;
-import com.hymin.webtoon_review.webtoon.mapper.ReplyMapper;
-import com.hymin.webtoon_review.webtoon.mapper.ReplyRecommendMapper;
+import com.hymin.webtoon_review.webtoon.exception.InvalidWebtoonRecommendException;
+import com.hymin.webtoon_review.webtoon.mapper.WebtoonMapper;
 import com.hymin.webtoon_review.webtoon.mapper.WebtoonRecommendMapper;
-import com.hymin.webtoon_review.webtoon.service.BookmarkService;
-import com.hymin.webtoon_review.webtoon.service.CommentRecommendService;
-import com.hymin.webtoon_review.webtoon.service.CommentService;
-import com.hymin.webtoon_review.webtoon.service.ReplyRecommendService;
-import com.hymin.webtoon_review.webtoon.service.ReplyService;
 import com.hymin.webtoon_review.webtoon.service.WebtoonRecommendService;
 import com.hymin.webtoon_review.webtoon.service.WebtoonService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WebtoonFacade {
 
+    @Value("${domain.thumbnail}")
+    private String domainThumbnail;
+
+    private final JobQueue jobQueue;
     private final UserService userService;
     private final WebtoonService webtoonService;
-    private final CommentService commentService;
-    private final BookmarkService bookmarkService;
     private final WebtoonRecommendService webtoonRecommendService;
-    private final ReplyService replyService;
-    private final CommentRecommendService commentRecommendService;
-    private final ReplyRecommendService replyRecommendService;
 
     @Transactional(readOnly = true)
-    public List<WebtoonInfo> getWentoonList(Authentication authentication, Pageable pageable,
-        String name, String lastValue, List<String> daysOfWeek,
-        List<String> platforms, List<String> genres
-    ) {
-        List<WebtoonInfo> webtoonInfoList = webtoonService.getWebtoonInfoList(authentication,
-            pageable, name, lastValue, daysOfWeek, platforms, genres);
-        List<Long> webtoonIdList = getWebtoonIdList(webtoonInfoList);
-        List<DayOfWeekSelectResult> dayOfWeekSelectResultList = webtoonService.getDayOfWeekSelectResultList(
-            webtoonIdList);
-        List<GenreSelectResult> genreSelectResultList = webtoonService.getGenreSelectResultList(
-            webtoonIdList);
-        List<AuthorSelectResult> authorSelectResultList = webtoonService.getAuthorSelectResultList(
-            webtoonIdList);
+    public List<WebtoonSimple> getWentoonList(Pageable pageable, String lastValue,
+        String daysOfWeek, String genre, String updatedAt) {
+        List<WebtoonSimple> webtoonSimpleList = webtoonService.getWebtoonList(
+            pageable,
+            lastValue,
+            daysOfWeek,
+            genre,
+            updatedAt
+        );
 
-        webtoonInfoList
-            .stream()
-            .forEach((webtoon) -> {
-                    webtoon.setDayOfWeeks(dayOfWeekSelectResultList
-                        .stream()
-                        .filter((dow) -> webtoon.getId().equals(dow.getWebtoonId()))
-                        .map(DayOfWeekSelectResult::getName)
-                        .toList()
-                    );
+        return setWebtoonSimpleList(webtoonSimpleList);
+    }
 
-                    webtoon.setGenres(genreSelectResultList
-                        .stream()
-                        .filter((genre) -> webtoon.getId().equals(genre.getWebtoonId()))
-                        .map(GenreSelectResult::getName)
-                        .toList());
-
-                    webtoon.setAuthorName(authorSelectResultList
-                        .stream()
-                        .filter((author) -> webtoon.getId().equals(author.getWebtoonId()))
-                        .map(AuthorSelectResult::getName)
-                        .toList());
-                }
-            );
-
-        return webtoonInfoList;
+    public List<WebtoonSimple> getHotWebtoonList() {
+        return setWebtoonSimpleList(webtoonService.getHotWebtoonList());
     }
 
     @Transactional
-    public void addBookmark(Authentication authentication, Long webtoonId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
+    public WebtoonDetails getWebtoonDetails(Authentication authentication, Long id) {
+        WebtoonDetails webtoonDetails = webtoonService.get(authentication.getName(), id);
+        jobQueue.add(TopicNames.view.name(), Job.of(id));
+        jobQueue.add(TopicNames.popularity.name(),
+            Job.of(WebtoonMapper.toWebtoonPopularityScore(id, 1)));
 
-        bookmarkService.save(BookmarkMapper.toBookmark(user, webtoon));
+        return WebtoonMapper.setWebtoonDetails(
+            webtoonDetails,
+            webtoonService.getDayOfWeeks(id),
+            webtoonService.getGenres(id),
+            webtoonService.getAuthors(id),
+            domainThumbnail
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> getWebtoonCategories() {
+        return WebtoonMapper.toWebtoonCategoryList(
+            webtoonService.getAllGenres()
+        );
     }
 
     @Transactional
-    public void removeBookmark(Authentication authentication, Long webtoonId, Long bookmarkId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Bookmark bookmark = bookmarkService.get(bookmarkId);
-
-        if (!bookmark.getUser().equals(user) ||
-            !bookmark.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        bookmarkService.delete(bookmark);
-    }
-
-    @Transactional
-    public void addRecommendation(Authentication authentication, Long webtoonId) {
+    public void addRecommend(Authentication authentication, Long webtoonId) {
         User user = userService.get(authentication.getName());
         Webtoon webtoon = webtoonService.get(webtoonId);
 
         webtoonRecommendService.save(WebtoonRecommendMapper.toWebtoonRecommend(user, webtoon));
+        webtoonService.increaseRecommendCount(webtoonId);
+        jobQueue.add(TopicNames.popularity.name(),
+            Job.of(WebtoonMapper.toWebtoonPopularityScore(webtoonId, 1)));
     }
 
     @Transactional
-    public void removeRecommendation(Authentication authentication, Long webtoonId,
-        Long recommendId) {
+    public void removeRecommend(Authentication authentication, Long webtoonId, Long recommendId) {
         User user = userService.get(authentication.getName());
         Webtoon webtoon = webtoonService.get(webtoonId);
         WebtoonRecommend webtoonRecommend = webtoonRecommendService.get(recommendId);
 
-        if (!webtoonRecommend.getUser().equals(user) ||
-            !webtoonRecommend.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
+        if (isInvalidWebtoonRecommend(user, webtoon, webtoonRecommend)) {
+            throw new InvalidWebtoonRecommendException(ResponseStatus.INVALID_WEBTOON_RECOMMEND);
         }
 
         webtoonRecommendService.delete(webtoonRecommend);
+        webtoonService.decreaseRecommendCount(webtoonId);
+        jobQueue.add(TopicNames.popularity.name(),
+            Job.of(WebtoonMapper.toWebtoonPopularityScore(webtoonId, -1)));
     }
 
-    @Transactional
-    public void addComment(Authentication authentication, Long id, CommentInfo commentInfo) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(id);
+    private List<WebtoonSimple> setWebtoonSimpleList(List<WebtoonSimple> webtoonSimpleList) {
+        List<Long> webtoonIdList = getWebtoonIdList(webtoonSimpleList);
 
-        commentService.save(CommentMapper.toComment(user, webtoon, commentInfo));
+        return WebtoonMapper.setWebtoonSimpleList(
+            webtoonSimpleList,
+            webtoonService.getDayOfWeekSelectResultList(webtoonIdList),
+            webtoonService.getGenreSelectResultList(webtoonIdList),
+            webtoonService.getAuthorSelectResultList(webtoonIdList),
+            domainThumbnail
+        );
     }
 
-    @Transactional
-    public void removeComment(Authentication authentication, Long webtoonId, Long commentId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-
-        if (!comment.getUser().equals(user) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        comment.delete();
-    }
-
-    @Transactional
-    public void addReply(Authentication authentication, Long webtoonId, Long commentId,
-        ReplyInfo replyInfo) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-
-        if (!comment.getUser().equals(user) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        replyService.save(ReplyMapper.toReply(user, comment, replyInfo));
-    }
-
-    @Transactional
-    public void removeReply(Authentication authentication, Long webtoonId, Long commentId,
-        Long replyId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-        Reply reply = replyService.get(replyId);
-
-        if (!reply.getUser().equals(user) ||
-            !reply.getComment().equals(comment) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        reply.delete();
-    }
-
-    @Transactional
-    public void addCommentRecommend(Authentication authentication, Long webtoonId, Long commentId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-
-        if (!comment.getUser().equals(user) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        commentRecommendService.save(CommentRecommendMapper.toCommentRecommend(user, comment));
-    }
-
-    @Transactional
-    public void removeCommentRecommend(Authentication authentication, Long webtoonId,
-        Long commentId, Long recommendId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-        CommentRecommend commentRecommend = commentRecommendService.get(recommendId);
-
-        if (!commentRecommend.getUser().equals(user) ||
-            !commentRecommend.getComment().equals(comment) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        commentRecommendService.delete(commentRecommend);
-    }
-
-    @Transactional
-    public void addReplyRecommend(Authentication authentication, Long webtoonId, Long commentId,
-        Long replyId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-        Reply reply = replyService.get(replyId);
-
-        if (!reply.getUser().equals(user) ||
-            !reply.getComment().equals(comment) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        replyRecommendService.save(ReplyRecommendMapper.toReplyRecommend(user, reply));
-    }
-
-    @Transactional
-    public void removeReplyRecommend(Authentication authentication, Long webtoonId, Long commentId,
-        Long replyId, Long recommendId) {
-        User user = userService.get(authentication.getName());
-        Webtoon webtoon = webtoonService.get(webtoonId);
-        Comment comment = commentService.get(commentId);
-        Reply reply = replyService.get(replyId);
-        ReplyRecommend replyRecommend = replyRecommendService.get(recommendId);
-
-        if (!replyRecommend.getUser().equals(user) ||
-            !replyRecommend.getReply().equals(reply) ||
-            !reply.getComment().equals(comment) ||
-            !comment.getWebtoon().equals(webtoon)) {
-            throw new GeneralException(ResponseStatus.BAD_REQUEST);
-        }
-
-        replyRecommendService.delete(replyRecommend);
-    }
-
-    private List<Long> getWebtoonIdList(List<WebtoonInfo> webtoonInfoList) {
-        return webtoonInfoList.stream()
-            .map(WebtoonInfo::getId)
+    private List<Long> getWebtoonIdList(List<WebtoonSimple> webtoonSimpleList) {
+        return webtoonSimpleList.stream()
+            .map(WebtoonSimple::getId)
             .toList();
+    }
+
+    private boolean isInvalidWebtoonRecommend(User user, Webtoon webtoon,
+        WebtoonRecommend webtoonRecommend) {
+        return !webtoonRecommend.getUser().equals(user) ||
+            !webtoonRecommend.getWebtoon().equals(webtoon);
     }
 }

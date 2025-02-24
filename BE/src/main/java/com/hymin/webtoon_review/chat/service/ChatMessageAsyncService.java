12f5a386a9d7403b2d999cb2f619961546ff87da
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-25 03:40:15 +0900

    feat: ChatMessageAsyncService 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/service/ChatMessageAsyncService.java
@@ -0,0 +1,30 @@
+package com.hymin.webtoon_review.chat.service;
+
+import com.hymin.webtoon_review.chat.dto.ChatRequest.ChatMessage;
+import com.hymin.webtoon_review.chat.repository.ChatMessageRepository;
+import com.hymin.webtoon_review.global.async.AsyncProcessor;
+import com.hymin.webtoon_review.global.async.Job;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChatMessageAsyncService implements AsyncProcessor {
+
+    ChatMessageRepository chatMessageRepository;
+
+    @Async
+    @Override
+    public void process(List<Job<?>> jobs) {
+        chatMessageRepository.saveAll(
+            jobs
+                .stream()
+                .map(job -> (ChatMessage) job.getData())
+                .toList()
+        );
+    }
+}

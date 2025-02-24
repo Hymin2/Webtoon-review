4dcecc17bd7a2a9fd9e7941f777fe556ed46823a
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-25 03:40:50 +0900

    feat, refactor: 채팅 관련 함수 이름 변경 및 메시지 전송 시 메시지 큐에 추가하도록 수정

--- BE/src/main/java/com/hymin/webtoon_review/chat/facade/ChatFacade.java
+++ BE/src/main/java/com/hymin/webtoon_review/chat/facade/ChatFacade.java
@@ -6,14 +6,17 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.teeing;
 
 import com.hymin.webtoon_review.chat.dto.ChatRequest.ChatMessage;
-import com.hymin.webtoon_review.chat.dto.ChatRequest.JoinExitMessage;
-import com.hymin.webtoon_review.chat.dto.ChatResponse.ChatReadCountInfo;
+import com.hymin.webtoon_review.chat.dto.ChatRequest.ConnectDisConnectMessage;
+import com.hymin.webtoon_review.chat.dto.ChatResponse.ChatRoomInfo;
 import com.hymin.webtoon_review.chat.entity.ChatRoom;
 import com.hymin.webtoon_review.chat.entity.UserChatRoom;
 import com.hymin.webtoon_review.chat.exception.InvalidChatRoomAccessException;
 import com.hymin.webtoon_review.chat.mapper.ChatMapper;
 import com.hymin.webtoon_review.chat.service.ChatService;
 import com.hymin.webtoon_review.chat.service.NotificationService;
+import com.hymin.webtoon_review.global.async.Job;
+import com.hymin.webtoon_review.global.async.JobQueue;
+import com.hymin.webtoon_review.global.async.TopicNames;
 import com.hymin.webtoon_review.user.entity.User;
 import com.hymin.webtoon_review.user.service.UserService;
 import java.util.Map;
@@ -26,6 +29,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChatFacade {
 
+    private final JobQueue jobQueue;
     private final UserService userService;
     private final ChatService chatService;
     private final NotificationService notificationService;
@@ -35,19 +39,21 @@ public class ChatFacade {
         simpleMessageSendingOperations
             .convertAndSend("/topic/chat/" + chatMessage.getRoomId(), chatMessage);
         notificationService.send(chatMessage);
+        jobQueue.add(TopicNames.chat.name(), Job.of(chatMessage));
     }
 
-    public void sendMessage(JoinExitMessage joinExitMessage) {
+    public void sendMessage(ConnectDisConnectMessage connectDisConnectMessage) {
         simpleMessageSendingOperations
-            .convertAndSend("/topic/chat/" + joinExitMessage.getRoomId(), joinExitMessage);
+            .convertAndSend("/topic/chat/" + connectDisConnectMessage.getRoomId(),
+                connectDisConnectMessage);
     }
 
-    public ChatReadCountInfo getReadCount(Long roomId, String username) {
+    public ChatRoomInfo getRoomInfoPrevConnect(Long roomId, String username) {
         if (chatService.existsRoomByUsername(roomId, username)) {
             throw new InvalidChatRoomAccessException();
         }
 
-        return ChatMapper.toChatReadCountInfo(roomId,
+        return ChatMapper.toChatRoomInfo(roomId,
             chatService.getUserChatRooms(roomId)
                 .stream()
                 .collect(teeing(

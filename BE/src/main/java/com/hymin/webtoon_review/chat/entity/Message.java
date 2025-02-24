99a1d49b01d425a040af69789e747246bda5ccb6
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-02-25 03:39:01 +0900

    feat: Message 엔티티 생성

---
+++ BE/src/main/java/com/hymin/webtoon_review/chat/entity/Message.java
@@ -0,0 +1,36 @@
+package com.hymin.webtoon_review.chat.entity;
+
+import com.hymin.webtoon_review.chat.enums.ChatType;
+import com.hymin.webtoon_review.global.BaseEntity;
+import com.hymin.webtoon_review.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message extends BaseEntity {
+
+    @Id
+    private String id;
+    private ChatType type;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+}

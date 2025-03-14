16e8c5211ecd61a6c5553e849d09f248c0edb040
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-03-15 04:20:15 +0900

    feat: 업로드 방식을 MultipartFile -> octet-stream 방식으로 변경, getFileUrl() 추가

--- BE/src/main/java/com/hymin/webtoon_review/file/controller/FileController.java
+++ BE/src/main/java/com/hymin/webtoon_review/file/controller/FileController.java
@@ -1,48 +1,56 @@
 package com.hymin.webtoon_review.file.controller;
 
 import com.hymin.webtoon_review.file.dto.FileRequest.FileUploadInfo;
-import com.hymin.webtoon_review.file.service.FileService;
+import com.hymin.webtoon_review.file.facade.FileFacade;
 import com.hymin.webtoon_review.global.response.ApiResponse;
 import com.hymin.webtoon_review.global.response.RestResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
 public class FileController {
 
-    private final FileService fileService;
+    private final FileFacade fileFacade;
 
     @GetMapping("/progress")
     public RestResponse getUploadProgress(@RequestParam("fileName") String fileName) {
-        return ApiResponse.onSuccess(fileService.getUploadProgress(fileName));
+        return ApiResponse.onSuccess(fileFacade.getUploadProgress(fileName));
+    }
+
+    @GetMapping("/url")
+    public RestResponse getFileUrl(@RequestParam("fileName") String fileName) {
+        return ApiResponse.onSuccess(fileFacade.getFileUrl(fileName));
     }
 
     @PostMapping("/start-upload")
     public RestResponse startUpload(@RequestBody FileUploadInfo fileUploadInfo) {
-        return ApiResponse.onSuccess(fileService.startUpload(fileUploadInfo));
+        return ApiResponse.onSuccess(fileFacade.startUpload(fileUploadInfo));
     }
 
-    @PostMapping("/chunk")
-    public RestResponse chunkUpload(@RequestParam("file") MultipartFile file,
-        String fileName,
-        int chunkSize,
-        int chunk) {
-        fileService.upload(file, fileName, chunkSize, chunk);
+    @PostMapping(value = "/chunk", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public RestResponse chunkUpload(HttpServletRequest request,
+        @RequestParam("fileName") String fileName,
+        @RequestParam("chunkSize") Integer chunkSize,
+        @RequestParam("chunk") Integer chunk) throws IOException {
+        fileFacade.upload(request.getInputStream(), fileName, chunkSize, chunk);
 
         return RestResponse.onCreated();
     }
 
-    @PostMapping
-    public RestResponse upload(@RequestParam("file") MultipartFile file) {
-        fileService.upload(file);
+    @PostMapping(consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public RestResponse upload(HttpServletRequest request,
+        @RequestParam("fileName") String fileName) throws IOException {
+        fileFacade.upload(request.getInputStream(), fileName);
 
         return RestResponse.onCreated();
     }

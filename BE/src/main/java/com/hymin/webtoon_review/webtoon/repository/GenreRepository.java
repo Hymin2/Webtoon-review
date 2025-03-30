114def59b8ad78aebed228353ac35e6da16ed936
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-03-30 09:26:30 +0900

    feat: 요일과 장르 조회 함수 수정

--- BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/GenreRepository.java
+++ BE/src/main/java/com/hymin/webtoon_review/webtoon/repository/GenreRepository.java
@@ -1,15 +1,12 @@
 package com.hymin.webtoon_review.webtoon.repository;
 
 import com.hymin.webtoon_review.webtoon.entity.Genre;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
-    @Query(value = "select g from Genre g inner join WebtoonGenre wg on wg.genre = g inner join Webtoon w on wg.webtoon = w where w.id =:id")
-    List<Genre> findByWebtoonId(@Param("id") Long id);
+    Optional<Genre> findByName(String name);
 }

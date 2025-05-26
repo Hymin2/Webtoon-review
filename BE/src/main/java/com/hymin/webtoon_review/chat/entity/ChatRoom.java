71ee6a35b6ace8bfb94b8c99ee95b80de4c899ae
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-05-26 20:36:05 +0900

    feat: 채팅방과 웹툰의 관계를 1:1로 수정

--- BE/src/main/java/com/hymin/webtoon_review/chat/entity/ChatRoom.java
+++ BE/src/main/java/com/hymin/webtoon_review/chat/entity/ChatRoom.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,7 +27,7 @@ public class ChatRoom {
 
     private String name;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "webtoon_id")
     private Webtoon webtoon;
 }

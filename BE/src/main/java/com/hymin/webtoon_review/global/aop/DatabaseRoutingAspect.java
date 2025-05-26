6b49ad401bb81d03f5021febb85dea142acddd3f
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-05-26 20:32:06 +0900

    feat: Queue의 작업이 실패했을 때 retry queue에 Job을 다시 넣는 AOP 기능 추가

--- BE/src/main/java/com/hymin/webtoon_review/global/aop/DatabaseRoutingAspect.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/aop/DatabaseRoutingAspect.java
@@ -1,18 +1,26 @@
 package com.hymin.webtoon_review.global.aop;
 
 import com.hymin.webtoon_review.global.DatabaseReadWriteRoutingDataSource;
+import com.hymin.webtoon_review.global.async.Job;
+import com.hymin.webtoon_review.global.async.JobQueue;
 import com.hymin.webtoon_review.global.enums.DataSourceEnum;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class DatabaseRoutingAspect {
+
+    private final JobQueue jobQueue;
 
     @Around("@annotation(org.springframework.transaction.annotation.Transactional)")
     public Object setReadOnlyConnection(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -38,6 +46,29 @@ public class DatabaseRoutingAspect {
             DatabaseReadWriteRoutingDataSource.setDataSourceKey(
                 DataSourceEnum.ASYNC_ONLY_WRITE.name());
             return joinPoint.proceed();
+        } catch (DataAccessException e) {
+            Object[] args = joinPoint.getArgs();
+
+            String topic = null;
+            List<Job<?>> jobs = null;
+
+            for (Object arg : args) {
+                if (arg instanceof String str) {
+                    topic = str;
+                } else if (
+                    arg instanceof List<?> list &&
+                        !list.isEmpty() &&
+                        list.get(0) instanceof Job
+                ) {
+                    jobs = (List<Job<?>>) list;
+                }
+            }
+
+            if (topic != null && jobs != null) {
+                jobQueue.addRetry(topic, jobs);
+            }
+
+            throw e;
         } finally {
             DatabaseReadWriteRoutingDataSource.setDataSourceKey(DataSourceEnum.WRITE.name());
         }

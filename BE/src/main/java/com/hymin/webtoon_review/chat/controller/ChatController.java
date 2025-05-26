feb3aa34bdcff60071ba61b899527837649a0eb4
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-05-26 20:39:51 +0900

    feat: 채팅 메시지 dto를 ChatMessage 객체로 통합하도록 수정
    - 입퇴장 시 사용하는 객체 ConnectDisConnectMessage를 삭제했습니다.

--- BE/src/main/java/com/hymin/webtoon_review/chat/controller/ChatController.java
+++ BE/src/main/java/com/hymin/webtoon_review/chat/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.hymin.webtoon_review.chat.controller;
 
 import com.hymin.webtoon_review.chat.dto.ChatRequest.ChatMessage;
-import com.hymin.webtoon_review.chat.dto.ChatRequest.ConnectDisConnectMessage;
 import com.hymin.webtoon_review.chat.facade.ChatFacade;
 import com.hymin.webtoon_review.global.annotation.Auth;
 import com.hymin.webtoon_review.global.response.ApiResponse;
@@ -11,9 +10,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,30 +22,39 @@ public class ChatController {
     private final ChatFacade chatFacade;
 
     @MessageMapping(value = "/chat/massages")
-    public void sendMessage(@RequestBody ChatMessage chatMessage,
-        SimpMessageHeaderAccessor headerAccessor) {
+    public void sendMessage(
+        @RequestBody ChatMessage chatMessage,
+        SimpMessageHeaderAccessor headerAccessor
+    ) {
         chatMessage.setSender((String) headerAccessor.getSessionAttributes().get("nickname"));
-
         chatFacade.sendMessage(chatMessage);
     }
 
-    @MessageMapping(value = "/chat/messages/connect-disconnect")
-    public void sendConnectDisconnectMessage(
-        @RequestBody ConnectDisConnectMessage connectDisConnectMessage) {
-        chatFacade.sendMessage(connectDisConnectMessage);
+    @MessageMapping(value = "/chat/messages/connect")
+    public void sendConnectMessage(@RequestBody ChatMessage chatMessage) {
+        chatFacade.sendConnectDisconnectMessage(chatMessage);
     }
 
-    @GetMapping("/chat/messages/{roomId}/info")
-    public RestResponse getRoomInfoPrevConnect(@PathVariable("roomId") Long roomId,
-        @Auth Authentication authentication) {
+    @MessageMapping(value = "/chat/messages/disconnect")
+    public void sendDisconnectMessage(@RequestBody ChatMessage chatMessage) {
+        chatFacade.sendConnectDisconnectMessage(chatMessage);
+    }
+
+    @GetMapping("/chat/room/info")
+    public RestResponse getRoomInfoPrevConnect(
+        @RequestParam("webtoonId") Long webtoonId,
+        @Auth Authentication authentication
+    ) {
         return ApiResponse.onSuccess(
-            chatFacade.getRoomInfoPrevConnect(roomId, authentication.getName()));
+            chatFacade.getRoomInfoPrevConnect(webtoonId, authentication.getName()));
     }
 
-    @PostMapping("/chat/room/{roomId}")
-    public RestResponse joinChatRoom(@PathVariable("roomId") Long roomId,
-        @Auth Authentication authentication) {
-        chatFacade.joinChatRoom(roomId, authentication.getName());
+    @PostMapping("/chat/room")
+    public RestResponse joinChatRoom(
+        @RequestParam("webtoonId") Long webtoonId,
+        @Auth Authentication authentication
+    ) {
+        chatFacade.joinChatRoom(webtoonId, authentication.getName());
 
         return RestResponse.onCreated();
     }

aecdf7a3788e9fa98945be39b70dfe312ab4fc44
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2025-05-26 20:28:52 +0900

    feat: JobQueue를 ArrayBlockingQueue로 변경

--- BE/src/main/java/com/hymin/webtoon_review/global/async/JobQueue.java
+++ BE/src/main/java/com/hymin/webtoon_review/global/async/JobQueue.java
@@ -3,18 +3,23 @@ package com.hymin.webtoon_review.global.async;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class JobQueue {
 
-    private final Map<String, ConcurrentLinkedQueue<Job<?>>> jobs;
+    private final Map<String, ArrayBlockingQueue<Job<?>>> jobs;
+    private final Map<String, ArrayBlockingQueue<Job<?>>> retries;
     private final Map<String, Integer> thresholds;
     private final Map<String, AsyncProcessor> processors;
 
-    private JobQueue(Map<String, ConcurrentLinkedQueue<Job<?>>> jobs,
-        Map<String, Integer> thresholds, Map<String, AsyncProcessor> processors) {
+    private JobQueue(
+        Map<String, ArrayBlockingQueue<Job<?>>> jobs,
+        Map<String, ArrayBlockingQueue<Job<?>>> retries,
+        Map<String, Integer> thresholds, Map<String, AsyncProcessor> processors
+    ) {
         this.jobs = jobs;
+        this.retries = retries;
         this.thresholds = thresholds;
         this.processors = processors;
     }
@@ -33,7 +38,30 @@ public class JobQueue {
         getQueue(topic).add(job);
     }
 
-    public void processAll(String topic) {
+    public void add(String topic, List<Job<?>> jobs) {
+        jobs.forEach(job -> add(topic, job));
+    }
+
+    public void addRetry(String topic, Job<?> job) {
+        getRetries(topic).add(job);
+    }
+
+    public void addRetry(String topic, List<Job<?>> jobs) {
+        jobs.forEach(job -> addRetry(topic, job));
+    }
+
+    public void retry(String topic) {
+        int size = getQueue(topic).size();
+        List<Job<?>> jobList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            jobList.add(getRetries(topic).poll());
+        }
+
+        processors.get(topic).process(jobList);
+    }
+
+    public void process(String topic) {
         int size = getQueue(topic).size();
         List<Job<?>> jobList = new ArrayList<>();
 
@@ -52,7 +80,7 @@ public class JobQueue {
         return getQueue(topic).size() >= thresholds.get(topic);
     }
 
-    private ConcurrentLinkedQueue<Job<?>> getQueue(String topic) {
+    private ArrayBlockingQueue<Job<?>> getQueue(String topic) {
         if (!jobs.containsKey(topic)) {
             throw new IllegalArgumentException("Topic " + topic + " does not exist");
         }
@@ -60,9 +88,18 @@ public class JobQueue {
         return jobs.get(topic);
     }
 
+    private ArrayBlockingQueue<Job<?>> getRetries(String topic) {
+        if (!retries.containsKey(topic)) {
+            throw new IllegalArgumentException("Topic " + topic + " does not exist");
+        }
+
+        return retries.get(topic);
+    }
+
     public static class JobQueueBuilder {
 
-        private final Map<String, ConcurrentLinkedQueue<Job<?>>> jobs = new ConcurrentHashMap<>();
+        private final Map<String, ArrayBlockingQueue<Job<?>>> jobs = new ConcurrentHashMap<>();
+        private final Map<String, ArrayBlockingQueue<Job<?>>> retries = new ConcurrentHashMap<>();
         private final Map<String, Integer> thresholds = new ConcurrentHashMap<>();
         private final Map<String, AsyncProcessor> processors = new ConcurrentHashMap<>();
 
@@ -73,8 +110,9 @@ public class JobQueue {
             if (jobs.containsKey(topic)) {
                 throw new IllegalArgumentException("Topic " + topic + " already exists");
             }
-            
-            jobs.put(topic, new ConcurrentLinkedQueue<>());
+
+            jobs.put(topic, new ArrayBlockingQueue<>(10000));
+            retries.put(topic, new ArrayBlockingQueue<>(10000));
             thresholds.put(topic, threshold);
             processors.put(topic, processor);
 
@@ -82,7 +120,7 @@ public class JobQueue {
         }
 
         public JobQueue build() {
-            return new JobQueue(jobs, thresholds, processors);
+            return new JobQueue(jobs, retries, thresholds, processors);
         }
     }
 }

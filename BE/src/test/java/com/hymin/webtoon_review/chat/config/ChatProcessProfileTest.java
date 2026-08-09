64f6765d5125a51bcde4e9eecbb74617e0a1333d
Hymin2 <khm8246@gmail.com>
2026-08-09 22:04:05 +0900

    test(chat): 백그라운드 프로세스 웹 서버 비활성화 검증

---
+++ BE/src/test/java/com/hymin/webtoon_review/chat/config/ChatProcessProfileTest.java
@@ -0,0 +1,34 @@
+package com.hymin.webtoon_review.chat.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+class ChatProcessProfileTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"chat-worker", "chat-persister"})
+    void 백그라운드_채팅_프로세스는_웹_서버_없이_실행된다(String profile) {
+        SpringApplication application = new SpringApplication(EmptyConfiguration.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.setLogStartupInfo(false);
+
+        try (ConfigurableApplicationContext context = application.run(
+            "--spring.profiles.active=" + profile
+        )) {
+            assertThat(context).isNotInstanceOf(WebServerApplicationContext.class);
+            assertThat(context.getEnvironment().getProperty("spring.main.web-application-type"))
+                .isEqualTo("none");
+        }
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    static class EmptyConfiguration {
+    }
+}
